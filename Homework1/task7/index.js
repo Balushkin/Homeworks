20b840d42a06394ca5ee3006cd00efa9b30381e3
Dima <110154392+Balushkin@users.noreply.github.com>
2022-07-31 00:37:35 +0300

    task7 done

---
+++ Homework1/task7/index.js
@@ -0,0 +1,7 @@
+let login = prompt('What is your login?');
+alert(`your login is - ${login}`);
+let password = prompt('What is your password');
+alert(`your password is - ${password}`);
+let email = prompt('What is your email?');
+alert(`your email is - ${email}`);
+alert(`Dear User your login is ${login}, yor password is ${password} , your email is${email}`);
