e6fb6e5c57f6afd00e241525e64381fe18bd0581
Dima <110154392+Balushkin@users.noreply.github.com>
2022-07-31 00:46:16 +0300

    task6 done

---
+++ Homework1/task6/index.js
@@ -0,0 +1,13 @@
+let myNane = 'Dima';
+let surrname = 'Balushko';
+let group = 'JS Fundamentals';
+let dateOfBirth = new Date(02,11,1198)
+let isMarried = false;
+console.log(typeof dateOfBirth);
+console.log(typeof isMarried);
+console.log(typeof group);
+console.log(typeof dateOfBirth);
+let weekend;
+console.log(typeof weekend);
+let child = null;
+console.log(typeof child);//task 6
